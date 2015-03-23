8f3ad7660db343f6adee88e0cc9c9e344bfa01f5
root <root@ubuntu-template.(none)>
2015-03-23 17:09:38 -0400

    qqqq

--- admin/installer/set_admin_pass.sql
+++
@@ -1,6 +0,0 @@
-use mysql;
-UPDATE user SET Password=PASSWORD("csip") WHERE User="root" AND Host="localhost";
-UPDATE user SET Password=PASSWORD("csip") WHERE User="root" AND Host="itsdev5.vip.gatech.edu";
-flush privileges;
-create database its;
-exit
